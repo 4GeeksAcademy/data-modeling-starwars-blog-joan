06fb78aafa9b43a275bd98e9c234bb6c0733fdc4
Joan <79165234+Joangb85@users.noreply.github.com>
2025-01-27 20:29:40 +0000

    Solution implemented

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import enum
 from sqlalchemy import ForeignKey, Integer, String
 from eralchemy2 import render_er
 from flask import Flask
@@ -10,48 +11,66 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
 app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    __tablename__ = 'User'
+class Planets(db.Model):
+    __tablename__ = 'Planets'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    ID = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(250), nullable=False, unique=True)
-    firstname = db.Column(db.String(250), nullable=False)
-    lastname = db.Column(db.String(250), nullable=False)
+    ID = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(250), nullable=False, unique=True)
+    population = db.Column(db.String(250), nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+    diameter = db.Column(db.String(250), nullable=False)
+    gravity = db.Column(db.String(250), nullable=False)
+
+class Films(db.Model):
+    __tablename__ = 'Films'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID = db.Column(db.Integer, primary_key=True, unique=True)
+    title = db.Column(db.String(250), nullable=False, unique=True)
+    episode_id = db.Column(db.String(250), nullable=False)
+    release_date = db.Column(db.String(250), nullable=False)
+    director = db.Column(db.String(250), nullable=False)
+    producer = db.Column(db.String(250), nullable=False)
+
+
+class People(db.Model):
+    __tablename__ = 'People'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID = db.Column(db.Integer, primary_key=True, unique=True)
+    name = db.Column(db.String(250), nullable=False, unique=True)
+    species = db.Column(db.String(250), nullable=False)
+    skin_color = db.Column(db.String(250), nullable=False)
+    hair_color = db.Column(db.String(250), nullable=False)
+    height = db.Column(db.String(250), nullable=False)
+    homeworld = db.Column(db.String(250), ForeignKey(Planets.ID), nullable=False)
+
+
+class Users(db.Model):
+    __tablename__ = 'Users'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    ID = db.Column(db.Integer, primary_key=True, unique=True)
+    first_name = db.Column(db.String(250), nullable=False)
+    last_name = db.Column(db.String(250), nullable=False)
     email = db.Column(db.String(250), nullable=False, unique=True)
+    username = db.Column(db.String(250), nullable=False, unique=True)
 
-class Post(db.Model):
-    __tablename__ = 'Post'
+class FavoriteTypeEnum(enum.Enum):
+    Planet= "Planet"
+    People = "People"
+    Films = "Films"
+
+class Favorites(db.Model):
+    __tablename__ = 'Favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    ID = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, ForeignKey(User.ID), nullable=False)
-
-
-class Media(db.Model):
-    __tablename__ = 'Media'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    ID = db.Column(db.Integer, primary_key=True)
-    type = db.Column(db.String(250), nullable=False)
-    url = db.Column(db.String(250), nullable=False)
-    post_id = db.Column(db.Integer, ForeignKey(Post.ID), nullable=False)
-
-class Comment(db.Model):
-    __tablename__ = 'Comment'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    ID = db.Column(db.Integer, primary_key=True)
-    comment_text = db.Column(db.String(250), nullable=False)
-    author_id = db.Column(db.Integer, ForeignKey(User.ID), nullable=False)
-    post_id = db.Column(db.Integer, ForeignKey(Post.ID), nullable=False)
-
-class Follower(db.Model):
-    __tablename__ = 'Follower'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    user_from_id = db.Column(db.Integer, ForeignKey(User.ID), primary_key=True, nullable=False)
-    user_to_id = db.Column(db.Integer, ForeignKey(User.ID), primary_key=True, nullable=False)
+    ID = db.Column(db.Integer, primary_key=True, unique=True)
+    User_id = db.Column(db.Integer, ForeignKey(Users.ID), nullable=False)
+    external_ID = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    type = db.Column(db.Enum(FavoriteTypeEnum), nullable=False, unique=True)
 
 
 
